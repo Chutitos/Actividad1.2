cc5365a539092d1a0a9f83748e3d28f9ab36d246
Chutitos <nomsnql@gmail.com>
2022-09-26 21:46:18 -0300

    0.2

--- app/src/main/java/com/example/actividad12/MainActivity.java
+++ app/src/main/java/com/example/actividad12/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.actividad12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button nuevaCuenta;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nuevaCuenta=(Button)findViewById(R.id.nuevaCuenta);
+        nuevaCuenta.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+
+                startActivity(new Intent (MainActivity.this, Registro.class));
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/actividad12/Registro.java
+++ app/src/main/java/com/example/actividad12/Registro.java
@@ -2,13 +2,40 @@ package com.example.actividad12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Registro extends AppCompatActivity {
+    EditText nombreUsuario;
+    Button  Registrarse;
+
+    public void enviarDatos (View view){
+        Bundle envioDatos = new Bundle();
+        envioDatos.putString("keyDatos", nombreUsuario.getText().toString());
+
+        Intent intent = new Intent(Registro.this, Feed.class);
+        intent.putExtras(envioDatos);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        nombreUsuario = findViewById(R.id.nombreUsuario);
+        Registrarse = findViewById(R.id.Registrarse);
+
+        Registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enviarDatos(view);
+            }
+        });
     }
+
+
 }
